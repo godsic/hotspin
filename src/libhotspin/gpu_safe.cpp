d82ae11c649436af32935fcc7888038d2c89af99
Mykola Dvornik <mykola.dvornik@gmail.com>
2014-03-20 15:23:32 +0100

    cast char* to avoid warning

--- src/libhotspin/gpu_safe.cpp
+++ src/libhotspin/gpu_safe.cpp
@@ -27,17 +27,17 @@ extern "C" {
 
 char* cufftGetErrorString(cufftResult s){
   switch(s){
-    default: return "Unknown error";
-    case CUFFT_SUCCESS: return "Any CUFFT operation is successful.";
-    case CUFFT_INVALID_PLAN: return "CUFFT is passed an invalid plan handle.";
-    case CUFFT_ALLOC_FAILED: return "CUFFT failed to allocate GPU memory.";
-    case CUFFT_INVALID_TYPE: return "The user requests an unsupported type.";
-    case CUFFT_INVALID_VALUE: return "The user specifies a bad memory pointer.";
-    case CUFFT_INTERNAL_ERROR: return "Used for all internal driver errors.";
-    case CUFFT_EXEC_FAILED: return "CUFFT failed to execute an FFT on the GPU.";
-    case CUFFT_SETUP_FAILED: return "The CUFFT library failed to initialize.";
+    default: return (char*)"Unknown error";
+    case CUFFT_SUCCESS: return (char*)"Any CUFFT operation is successful.";
+    case CUFFT_INVALID_PLAN: return (char*)"CUFFT is passed an invalid plan handle.";
+    case CUFFT_ALLOC_FAILED: return (char*)"CUFFT failed to allocate GPU memory.";
+    case CUFFT_INVALID_TYPE: return (char*)"The user requests an unsupported type.";
+    case CUFFT_INVALID_VALUE: return (char*)"The user specifies a bad memory pointer.";
+    case CUFFT_INTERNAL_ERROR: return (char*)"Used for all internal driver errors.";
+    case CUFFT_EXEC_FAILED: return (char*)"CUFFT failed to execute an FFT on the GPU.";
+    case CUFFT_SETUP_FAILED: return (char*)"The CUFFT library failed to initialize.";
 //     case CUFFT_SHUTDOWN_FAILED: return "The CUFFT library failed to shut down.";
-    case CUFFT_INVALID_SIZE: return "The user specifies an unsupported FFT size.";
+    case CUFFT_INVALID_SIZE: return (char*)"The user specifies an unsupported FFT size.";
   }
 }
 
