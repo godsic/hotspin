6a246db75a5f0bae182ed49e03ff0b8a2c184ff8
Mykola Dvornik <mykola.dvornik@gmail.com>
2014-04-10 16:32:37 +0200

    rework demag convolution v1

--- src/hotspin-core/modules/maxwell_convolution.go
+++ src/hotspin-core/modules/maxwell_convolution.go
@@ -15,14 +15,12 @@ package modules
 // Author: Arne Vansteenkiste
 
 import (
-	"fmt"
-	"math"
 	. "hotspin-core/common"
 	. "hotspin-core/engine"
 	"hotspin-core/gpu"
 	"hotspin-core/host"
+	"math"
 	"runtime"
-	"unsafe"
 )
 
 // Full Maxwell Electromagnetic field solver.
@@ -32,47 +30,14 @@ type MaxwellPlan struct {
 	dataSize    [3]int           // Size of the (non-zero) input data block (engine.GridSize)
 	logicSize   [3]int           // Non-transformed kernel size >= dataSize (due to zeropadding)
 	fftKernSize [3]int           // transformed kernel size, non-redundant parts only
-	kern        [3]*host.Array   // Real-space kernels for charge, dipole, rotor
-	fftKern     [7][3]*gpu.Array // transformed kernel's non-redundant parts (only real or imag parts, or nil)
-	fftMul      [7][3]complex128 // multipliers for kernel
-	fftBuffer   gpu.Array        // transformed input data
-	fftOut      gpu.Array        // transformed output data (3-comp)
+	kern        *host.Array      // Real-space kernels for charge
+	fftKern     [3][3]*gpu.Array // transformed kernel's non-redundant parts (only real or imag parts, or nil)
+	fftMul      [3][3]complex128 // multipliers for kernel
+	fftBuf      *gpu.Array       // transformed input data
 	fftPlan     gpu.FFTInterface // transforms input/output data
-	EInput      [7]*gpu.Array    // input quantities for electric field (rho, Px, Py, Pz, 0, 0, 0)
-	BInput      [7]*gpu.Array    // input quantities for magnetic field (rhoB, mx, my, mz, jx, jy, jz)
-	EInMul      [7]float64       // E input multipliers (epsillon0 etc)
-	BInMul      [7]float64       // B input multipliers (mu0 etc)
-	//BExt, EExt  *Quant           // external B/E field
-	dBdt, dEdt *Quant // time derivative of field
-	E, B       *Quant // E/B fields
-	// TODO: time derivatives could be taken in FFT space, but this complicates external fields
-	//fftE1, fftE2 *gpu.Array       // previous FFT E fields for time derivative
-	//fftB1, fftB2 *gpu.Array       // previous FFT B fields for time derivative
-	//time1, time2 float64          // time of previous fields for derivative
+	B           *Quant
 }
 
-// Index for kern
-const (
-	CHARGE = 0
-	DIPOLE = 1
-	ROTOR  = 2
-)
-
-// Index for EInput, BInput, EInMul, BInMul
-const (
-	RHO = 0
-	PX  = 1
-	MX  = 1
-	PY  = 2
-	MY  = 2
-	PZ  = 3
-	MZ  = 3
-	JX  = 4
-	JY  = 5
-	JZ  = 6
-)
-
-//
 func (plan *MaxwellPlan) init() {
 	if plan.initialized {
 		return
@@ -90,8 +55,7 @@ func (plan *MaxwellPlan) init() {
 
 	// init fft
 	fftOutputSize := gpu.FFTOutputSize(logicSize)
-	plan.fftBuffer.Init(1, fftOutputSize, gpu.DO_ALLOC) // TODO: recycle
-	plan.fftOut.Init(3, fftOutputSize, gpu.DO_ALLOC)    // TODO: recycle
+	plan.fftBuf = gpu.NewArray(3, fftOutputSize)
 	plan.fftPlan = gpu.NewDefaultFFT(dataSize, logicSize)
 
 	// init fftKern
@@ -114,7 +78,7 @@ const (
 // Load dipole kernel if not yet done so.
 // Required for field of electric/magnetic charge density.
 func (plan *MaxwellPlan) loadDipoleKernel() {
-	if plan.kern[DIPOLE] != nil {
+	if plan.kern != nil {
 		return
 	}
 	e := GetEngine()
@@ -127,98 +91,47 @@ func (plan *MaxwellPlan) loadDipoleKernel() {
 	kern := quant.Buffer(FIELD)
 	accuracy := 6
 	Kernel_Arne(plan.logicSize[:], e.CellSize(), e.Periodic(), accuracy, kern)
-	plan.kern[DIPOLE] = kern
-	plan.LoadKernel(kern, 1, SYMMETRIC, PUREREAL)
-}
-
-// Calculate the electric field plan.E
-func (plan *MaxwellPlan) UpdateE() {
-	plan.update(&plan.EInput, &plan.EInMul, plan.E.Array(), nil) //plan.EExt)
+	plan.kern = kern
+	plan.LoadKernel(kern, SYMMETRIC, PUREREAL)
 }
 
 // Calculate the magnetic field plan.B
 func (plan *MaxwellPlan) UpdateB() {
-	var normM *gpu.Array
-	hasMsat := 0
 
 	msat0T0 := GetEngine().Quant("Msat0T0")
 	mf := GetEngine().Quant("mf")
 
+	M := gpu.NewArray(3, mf.Array().Size3D())
+
+	M.CopyFromDevice(mf.Array())
+
 	if !msat0T0.Array().IsNil() {
-		hasMsat = 1
-
-		sx := mf.Array().Size3D()[X]
-		sy := mf.Array().Size3D()[Y]
-		sz := mf.Array().Size3D()[Z]
-
-		normM = gpu.NewArray(3, []int{sx, sy, sz})
-
-		gpu.Mul(normM.Component(X), mf.Array().Component(X), msat0T0.Array())
-		gpu.Mul(normM.Component(Y), mf.Array().Component(Y), msat0T0.Array())
-		gpu.Mul(normM.Component(Z), mf.Array().Component(Z), msat0T0.Array())
-
-		plan.BInput[MX] = normM.Component(X)
-		plan.BInput[MY] = normM.Component(Y)
-		plan.BInput[MZ] = normM.Component(Z)
-	} else {
-		plan.BInput[MX] = mf.Array().Component(X)
-		plan.BInput[MY] = mf.Array().Component(Y)
-		plan.BInput[MZ] = mf.Array().Component(Z)
+		gpu.Mul(M.Component(X), M.Component(X), msat0T0.Array())
+		gpu.Mul(M.Component(Y), M.Component(Y), msat0T0.Array())
+		gpu.Mul(M.Component(Z), M.Component(Z), msat0T0.Array())
 	}
 
-	plan.BInMul[MX] = msat0T0.Multiplier()[0] * Mu0
-	plan.BInMul[MY] = msat0T0.Multiplier()[0] * Mu0
-	plan.BInMul[MZ] = msat0T0.Multiplier()[0] * Mu0
+	mMul := [3]float64{msat0T0.Multiplier()[0] * Mu0, msat0T0.Multiplier()[0] * Mu0, msat0T0.Multiplier()[0] * Mu0}
 
-	plan.update(&plan.BInput, &plan.BInMul, plan.B.Array(), nil)
-	if hasMsat == 1 {
-		normM.Free()
-	}
+	plan.ForwardFFT(M)
+
+	// Point-wise kernel multiplication
+	gpu.TensSYMMVecMul(&plan.fftBuf.Comp[X], &plan.fftBuf.Comp[Y], &plan.fftBuf.Comp[Z],
+		&plan.fftBuf.Comp[X], &plan.fftBuf.Comp[Y], &plan.fftBuf.Comp[Z],
+		plan.fftKern[X][X], plan.fftKern[Y][Y], plan.fftKern[Z][Z],
+		plan.fftKern[Y][Z], plan.fftKern[X][Z], plan.fftKern[X][Y],
+		mMul[X], mMul[Y], mMul[Z],
+		plan.fftKernSize[X], plan.fftKernSize[Y], plan.fftKernSize[Z],
+		plan.fftBuf.Stream)
+
+	plan.InverseFFT(plan.B.Array())
+
+	M.Free()
 }
 
-// calculate E or B
-func (plan *MaxwellPlan) update(in *[7]*gpu.Array, inMul *[7]float64, out *gpu.Array, ext *Quant) {
-	fftBuf := &plan.fftBuffer
-	fftOut := &plan.fftOut
-	fftOut.Zero() // TODO: smarter kernel mul that does not read/add/read/add/...
-	needInverseFFT := false
-	for i := 0; i < 7; i++ {
-		if in[i] == nil {
-			continue
-		}
-		//fmt.Println("in", i, in[i].LocalCopy().Array)
-		plan.ForwardFFT(in[i])
-		for j := 0; j < 3; j++ {
-			if plan.fftKern[i][j] == nil {
-				continue
-			}
-			// Point-wise kernel multiplication
-			mul := complex128(complex(inMul[i], 0) * plan.fftMul[i][j])
-			//Debug(i, j, "mul", mul)
-			gpu.CMaddAsync(&fftOut.Comp[j], mul, plan.fftKern[i][j], fftBuf, fftOut.Stream)
-			needInverseFFT = true
-			fftOut.Stream.Sync()
-		}
-	}
-	if needInverseFFT {
-		plan.InverseFFT(out)
-	} else {
-		out.Zero()
-	}
-	// add external field
-	if ext != nil {
-		for c := 0; c < 3; c++ {
-			mul := ext.Multiplier()[c]
-			if mul != 0 {
-				gpu.Madd(out.Component(c), out.Component(c), ext.Array().Component(c), mul)
-			}
-		}
-	}
-}
-
-//// Loads a sub-kernel at position pos in the 3x7 global kernel matrix.
+//// Loads a sub-kernel at position pos in the 3x3 global kernel matrix.
 //// The symmetry and real/imaginary/complex properties are taken into account to reduce storage.
-func (plan *MaxwellPlan) LoadKernel(kernel *host.Array, pos int, matsymm int, realness int) {
+func (plan *MaxwellPlan) LoadKernel(kernel *host.Array, matsymm int, realness int) {
 
 	//	for i := range kernel.Array {
 	//		Debug("kernel", TensorIndexStr[i], ":", kernel.Array[i], "\n\n\n")
@@ -268,8 +181,8 @@ func (plan *MaxwellPlan) LoadKernel(kernel *host.Array, pos int, matsymm int, re
 		}
 
 		// clear data first
-		AssertMsg(plan.fftKern[i+pos][j] == nil, "I'm afraid I can't let you overwrite that")
-		AssertMsg(plan.fftMul[i+pos][j] == 0, "Likewise")
+		AssertMsg(plan.fftKern[i][j] == nil, "I'm afraid I can't let you overwrite that")
+		AssertMsg(plan.fftMul[i][j] == 0, "Likewise")
 
 		// ignore zeros
 		if k < kernel.NComp() && IsZero(kernel.Comp[k], max) {
@@ -280,13 +193,13 @@ func (plan *MaxwellPlan) LoadKernel(kernel *host.Array, pos int, matsymm int, re
 		// auto-fill lower triangle if possible
 		if k > XY {
 			if matsymm == SYMMETRIC {
-				plan.fftKern[i+pos][j] = plan.fftKern[j+pos][i]
-				plan.fftMul[i+pos][j] = plan.fftMul[j+pos][i]
+				plan.fftKern[i][j] = plan.fftKern[j][i]
+				plan.fftMul[i][j] = plan.fftMul[j][i]
 				continue
 			}
 			if matsymm == ANTISYMMETRIC {
-				plan.fftKern[i+pos][j] = plan.fftKern[j+pos][i]
-				plan.fftMul[i+pos][j] = -plan.fftMul[j+pos][i]
+				plan.fftKern[i][j] = plan.fftKern[j][i]
+				plan.fftMul[i][j] = -plan.fftMul[j][i]
 				continue
 			}
 		}
@@ -300,24 +213,14 @@ func (plan *MaxwellPlan) LoadKernel(kernel *host.Array, pos int, matsymm int, re
 		// extract real or imag parts
 		hostFFTKern := extract(hostOut, realness)
 		rescale(hostFFTKern, 1/float64(gpu.FFTNormLogic(logic)))
-		plan.fftKern[i+pos][j] = gpu.NewArray(1, hostFFTKern.Size3D)
-		plan.fftKern[i+pos][j].CopyFromHost(hostFFTKern)
-		plan.fftMul[i+pos][j] = scaling
+		plan.fftKern[i][j] = gpu.NewArray(1, hostFFTKern.Size3D)
+		plan.fftKern[i][j].CopyFromHost(hostFFTKern)
+		plan.fftMul[i][j] = scaling
 	}
 
-	// debug
-	if DEBUG {
-		var dbg [7][3]string
-		for i := range dbg {
-			for j := range dbg[i] {
-				dbg[i][j] = fmt.Sprint(unsafe.Pointer(plan.fftKern[i][j]), "*", plan.fftMul[i][j])
-			}
-		}
-		Debug("maxwell convplan kernel:", dbg)
-	}
 }
 
-const zero_tolerance = 1e-5
+const zero_tolerance = 1e-12
 
 // list is considered zero if all elements are
 // at least a factorzero_tolerance smaller than max.
@@ -421,60 +324,21 @@ func (plan *MaxwellPlan) Free() {
 // Sparse transform all 3 components.
 // (FFTPlan knows about zero padding etc)
 func (plan *MaxwellPlan) ForwardFFT(in *gpu.Array) {
-	Assert(plan.fftBuffer.NComp() == in.NComp())
-	Assert(plan.fftBuffer.NComp() == 1)
-	//for c := range in.Comp {
-	plan.fftPlan.Forward(in, &plan.fftBuffer)
-	//}
+	Assert(plan.fftBuf.NComp() == in.NComp())
+	for c := range in.Comp {
+		plan.fftPlan.Forward(&in.Comp[c], &plan.fftBuf.Comp[c])
+	}
 }
 
 // 	INTERNAL
 // Sparse backtransform
 // (FFTPlan knows about zero padding etc)
 func (plan *MaxwellPlan) InverseFFT(out *gpu.Array) {
-	Assert(plan.fftOut.NComp() == out.NComp())
+	Assert(plan.fftBuf.NComp() == out.NComp())
 	for c := range out.Comp {
-		plan.fftPlan.Inverse(&plan.fftOut.Comp[c], &out.Comp[c])
+		plan.fftPlan.Inverse(&plan.fftBuf.Comp[c], &out.Comp[c])
 	}
 }
-
-//func (conv *Conv73Plan) SelfTest() {
-//	Debug("FFT self-test")
-//	rng := rand.New(rand.NewSource(0))
-//	size := conv.dataSize[:]
-//
-//	in := NewArray(1, size)
-//	defer in.Free()
-//	arr := in.LocalCopy()
-//	a := arr.List
-//	for i := range a {
-//		a[i] = 2*rng.Float32() - 1
-//		if a[i] == 0 {
-//			a[i] = 1
-//		}
-//	}
-//	in.CopyFromHost(arr)
-//
-//	out := NewArray(1, size)
-//	defer out.Free()
-//
-//	conv.ForwardFFT(in)
-//	conv.InverseFFT(out)
-//
-//	b := out.LocalCopy().List
-//	norm := float64(1 / float64(FFTNormLogic(conv.logicSize[:])))
-//	var maxerr float64
-//	for i := range a {
-//		if Abs32(a[i]-b[i]*norm) > maxerr {
-//			maxerr = Abs32(a[i] - b[i]*norm)
-//		}
-//	}
-//	Debug("FFT max error:", maxerr)
-//	if maxerr > 1e-3 {
-//		panic(BugF("FFT self-test failed, max error:", maxerr, "\nPlease use a different grid size of FFT type."))
-//	}
-//	runtime.GC()
-//}
 
 // Extract real or imaginary parts, copy them from src to dst.
 // In the meanwhile, check if the other parts are nearly zero
@@ -513,7 +377,7 @@ func extract(src *host.Array, realness int) *host.Array {
 	Debug("FFT Kernel max part", realness, ":", maxgood)
 	Debug("FFT Kernel max part", other, ":", maxbad)
 	Debug("FFT Kernel max bad/good part=", maxbad/maxgood)
-	if maxbad/maxgood > 1e-5 { // TODO: is this reasonable?
+	if maxbad/maxgood > 1e-12 { // TODO: is this reasonable?
 		panic(BugF("FFT Kernel max bad/good part=", maxbad/maxgood))
 	}
 	return dst
