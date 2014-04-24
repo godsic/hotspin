3709821573393baaf0df9eb2cd9efa8182dad89d
Mykola Dvornik <mykola.dvornik@gmail.com>
2014-04-24 14:25:39 +0200

    improve phonon's heat capacity unit test

--- tests/Cp.py
+++ tests/Cp.py
@@ -15,9 +15,9 @@ hsX = 0.5 * sX
 hsY = 0.5 * sY
 hsZ = 0.5 * sZ
 
-csX = sX/Nx
-csY = sY/Ny
-csZ = sZ/Nz
+csX = sX / Nx
+csY = sY / Ny
+csZ = sZ / Nz
 
 setgridsize(Nx, Ny, Nz)
 setcellsize(csX, csY, csZ)
@@ -32,17 +32,17 @@ Ms0 = 516967
 J = 0.5
 g = 2.19
 g_LL = g * mu0 * muB * 2.0 * pi / 6.62606896e-34
-print g_LL
+echo(str(g_LL))
 ro = Ms0 / (g * muB * J)
-Tc= 633
+Tc = 633
 
 setv('Td', 390.0)
 setv('n', ro)
 
-Ta=5.0
-Tb=1800.0
-steps=10000
-dT = (Tb-Ta)/steps
+Ta = 0
+Tb = 1800.0
+steps = 10000
+dT = (Tb - Ta) / steps
 
 savegraph("graph.png")
 
@@ -53,11 +53,11 @@ f = open('Cp.dat', 'w')
 
 for i in range(steps):
 
-    Temp = Ta + float(i)*dT
+    Temp = Ta + float(i) * dT
     T = [[[[Temp]]]]
     setarray('Tl', T)
     cp = getcell("Cp_l", 0, 0, 0)[0]
-    print cp
+    echo(str(cp))
     out = str(Temp) + '\t' + str(cp) + '\n'
     f.write(out)
 
