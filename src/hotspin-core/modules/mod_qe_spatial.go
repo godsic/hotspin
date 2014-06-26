35161da6e388a072785cc3b4dd66bc77a260bf6d
Mykola Dvornik <mykola.dvornik@gmail.com>
2014-06-26 15:47:08 +0200

    cleanup TM related stuff

--- src/hotspin-core/modules/mod_qe_spatial.go
+++ src/hotspin-core/modules/mod_qe_spatial.go
@@ -2,22 +2,20 @@
 //  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
 //  Use of this source code is governed by the GNU General Public License version 3
 //  (as published by the Free Software Foundation) that can be found in the license.txt file.
-//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  Note that you are welcome to modify this code under the condition that you do not remove any
 //  copyright notices and prominently state that you modified it, giving a relevant date.
 
 package modules
 
 import (
-	//~ . "hotspin-core/common"
 	. "hotspin-core/engine"
 )
 
 // Register this module
 func init() {
-	RegisterModule("temperature/ETM/Qspatial", "Electrons: Heat flow caused by spatial temperature gradient", LoadQEspat)
+	RegisterModule("temperature/ETM/diffusion", "Electrons heat diffusion", LoadQEspat)
 }
 
 func LoadQEspat(e *Engine) {
 	LoadQspat(e, EtempName, EspatFluxName, EcondName)
-	//AddTermToQuant(e.Quant("Q"), Qspat)
 }
--- src/hotspin-core/modules/mod_qs_spatial.go
+++ src/hotspin-core/modules/mod_qs_spatial.go
@@ -2,22 +2,20 @@
 //  Copyright 2011  Arne Vansteenkiste and Ben Van de Wiele.
 //  Use of this source code is governed by the GNU General Public License version 3
 //  (as published by the Free Software Foundation) that can be found in the license.txt file.
-//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  Note that you are welcome to modify this code under the condition that you do not remove any
 //  copyright notices and prominently state that you modified it, giving a relevant date.
 
 package modules
 
 import (
-	//~ . "hotspin-core/common"
 	. "hotspin-core/engine"
 )
 
 // Register this module
 func init() {
-	RegisterModule("temperature/STM/Qspatial", "Spins: Heat flow caused by spatial temperature gradient", LoadQSspat)
+	RegisterModule("temperature/STM/diffusion", "Spins heat diffusion", LoadQSspat)
 }
 
 func LoadQSspat(e *Engine) {
 	LoadQspat(e, StempName, SspatFluxName, ScondName)
-	//AddTermToQuant(e.Quant("Q"), Qspat)
 }
