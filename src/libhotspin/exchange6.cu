57c96f02eb0edb7a209fcf10ebe6d7990686cf94
godsic <mykola.dvornik@gmail.com>
2014-07-25 22:09:34 +0200

    change exchange boundary conditions

--- src/libhotspin/exchange6.cu
+++ src/libhotspin/exchange6.cu
@@ -26,16 +26,15 @@ __global__ void exchange6Kern(double* __restrict__ hx, double* __restrict__  hy,
 
         int I = i * N1 * N2 + j * N2 + k;
 
-        double lex02 = getMaskUnity(lexMsk, I) * getMaskUnity(lexMsk, I);
+        double lex = getMaskUnity(lexMsk, I);
+        double lex02 =  lex * lex;
         double lex2, pre1, pre2;
 
-        double3 m0 = make_double3(mx[I], my[I], mz[I]);
-        double ms0 = len(m0) * getMaskUnity(msat0T0Msk, I);
-        double3 s0 = normalize(m0);
+        double ms0 = getMaskUnity(msat0T0Msk, I);
+        double3 m0 = make_double3(mx[I] * ms0, my[I] * ms0, mz[I] * ms0);
 
         double Hx, Hy, Hz;
         double ms2, ms1;
-        double3 s1, s2;
         double3 m1, m2;
 
         int linAddr;
@@ -46,57 +45,57 @@ __global__ void exchange6Kern(double* __restrict__ hx, double* __restrict__  hy,
         idx = max(idx, 0);
         linAddr = idx * N1 * N2 + j * N2 + k;
     
-        m1 = make_double3(mx[linAddr], my[linAddr], mz[linAddr]);
-        ms1 = len(m1) * getMaskUnity(msat0T0Msk, linAddr);
-        s1 = normalize(m1);
+        ms1 = getMaskUnity(msat0T0Msk, linAddr);
+        m1 = make_double3(mx[linAddr] * ms1, my[linAddr] * ms1, mz[linAddr] * ms1);
 
-        lex2 = getMaskUnity(lexMsk, linAddr) * getMaskUnity(lexMsk, linAddr);
-        pre1 = avgGeomZero(lex02 * ms0, lex2 * ms1);
+        lex = getMaskUnity(lexMsk, linAddr);
+        lex2 =  lex * lex;
+        pre1 = avgGeomZero(lex02, lex2);
 
         idx = i + 1;
         idx = (idx == N0 && wrap0) ? idx - N0 : idx;
         idx = min(idx, N0 - 1);
         linAddr = idx * N1 * N2 + j * N2 + k;
 
-        m2 = make_double3(mx[linAddr], my[linAddr], mz[linAddr]);
-        ms2 = len(m2) * getMaskUnity(msat0T0Msk, linAddr);;
-        s2 = normalize(m2);
+        ms2 = getMaskUnity(msat0T0Msk, linAddr);
+        m2 = make_double3(mx[linAddr] * ms2, my[linAddr] * ms2, mz[linAddr] * ms2);
 
-        lex2 = getMaskUnity(lexMsk, linAddr) * getMaskUnity(lexMsk, linAddr);
-        pre2 = avgGeomZero(lex02 * ms0, lex2 * ms2);
+        lex = getMaskUnity(lexMsk, linAddr);
+        lex2 =  lex * lex;
+        pre2 = avgGeomZero(lex02, lex2);
 
-        Hx = lex2Mul_cellSizeX2 * (pre1 * (s1.x - s0.x) + pre2 * (s2.x - s0.x));
-        Hy = lex2Mul_cellSizeX2 * (pre1 * (s1.y - s0.y) + pre2 * (s2.y - s0.y));
-        Hz = lex2Mul_cellSizeX2 * (pre1 * (s1.z - s0.z) + pre2 * (s2.z - s0.z));
+        Hx = lex2Mul_cellSizeX2 * (pre1 * (m1.x - m0.x) + pre2 * (m2.x - m0.x));
+        Hy = lex2Mul_cellSizeX2 * (pre1 * (m1.y - m0.y) + pre2 * (m2.y - m0.y));
+        Hz = lex2Mul_cellSizeX2 * (pre1 * (m1.z - m0.z) + pre2 * (m2.z - m0.z));
 
         // neighbors in Z direction
         idx = k - 1;
         idx = (idx < 0 && wrap2) ? N2 + idx : idx;
         idx = max(idx, 0);
         linAddr = i * N1 * N2 + j * N2 + idx;
-    
-        m1 = make_double3(mx[linAddr], my[linAddr], mz[linAddr]);
-        ms1 = len(m1) * getMaskUnity(msat0T0Msk, linAddr);
-        s1 = normalize(m1);
 
-        lex2 = getMaskUnity(lexMsk, linAddr) * getMaskUnity(lexMsk, linAddr);
-        pre1 = avgGeomZero(lex02 * ms0, lex2 * ms1);
+        ms1 = getMaskUnity(msat0T0Msk, linAddr);
+        m1 = make_double3(mx[linAddr] * ms1, my[linAddr] * ms1, mz[linAddr] * ms1);
+
+        lex = getMaskUnity(lexMsk, linAddr);
+        lex2 =  lex * lex;
+        pre1 = avgGeomZero(lex02, lex2);
 
         idx = k + 1;
         idx = (idx == N2 && wrap2) ? idx - N2 : idx;
         idx = min(idx, N2 - 1);
         linAddr = i * N1 * N2 + j * N2 + idx;
 
-        m2 = make_double3(mx[linAddr], my[linAddr], mz[linAddr]);
-        ms2 = len(m2) * getMaskUnity(msat0T0Msk, linAddr);
-        s2 = normalize(m2);
+        ms2 = getMaskUnity(msat0T0Msk, linAddr);
+        m2 = make_double3(mx[linAddr] * ms2, my[linAddr] * ms2, mz[linAddr] * ms2);
 
-        lex2 = getMaskUnity(lexMsk, linAddr) * getMaskUnity(lexMsk, linAddr);
-        pre2 = avgGeomZero(lex02 * ms0, lex2 * ms2);
+        lex = getMaskUnity(lexMsk, linAddr);
+        lex2 =  lex * lex;
+        pre2 = avgGeomZero(lex02, lex2);
 
-        Hx += lex2Mul_cellSizeZ2 * (pre1 * (s1.x - s0.x) + pre2 * (s2.x - s0.x));
-        Hy += lex2Mul_cellSizeZ2 * (pre1 * (s1.y - s0.y) + pre2 * (s2.y - s0.y));
-        Hz += lex2Mul_cellSizeZ2 * (pre1 * (s1.z - s0.z) + pre2 * (s2.z - s0.z));
+        Hx += lex2Mul_cellSizeZ2 * (pre1 * (m1.x - m0.x) + pre2 * (m2.x - m0.x));
+        Hy += lex2Mul_cellSizeZ2 * (pre1 * (m1.y - m0.y) + pre2 * (m2.y - m0.y));
+        Hz += lex2Mul_cellSizeZ2 * (pre1 * (m1.z - m0.z) + pre2 * (m2.z - m0.z));
 
         // neighbors in Y direction
         idx = j - 1;
@@ -104,28 +103,28 @@ __global__ void exchange6Kern(double* __restrict__ hx, double* __restrict__  hy,
         idx = max(idx, 0);
         linAddr = i * N1 * N2 + idx * N2 + k;
 
-        m1 = make_double3(mx[linAddr], my[linAddr], mz[linAddr]);
-        ms1 = len(m1) * getMaskUnity(msat0T0Msk, linAddr);
-        s1 = normalize(m1);
+        ms1 = getMaskUnity(msat0T0Msk, linAddr);
+        m1 = make_double3(mx[linAddr] * ms1, my[linAddr] * ms1, mz[linAddr] * ms1);
 
-        lex2 = getMaskUnity(lexMsk, linAddr) * getMaskUnity(lexMsk, linAddr);
-        pre1 = avgGeomZero(lex02 * ms0, lex2 * ms1);
+        lex = getMaskUnity(lexMsk, linAddr);
+        lex2 =  lex * lex;
+        pre1 = avgGeomZero(lex02, lex2);
 
         idx = j + 1;
         idx = (idx == N1 && wrap1) ? idx - N1 : idx;
         idx = min(idx, N1 - 1);
         linAddr = i * N1 * N2 + idx * N2 + k;
 
-        m2 = make_double3(mx[linAddr], my[linAddr], mz[linAddr]);
-        ms2 = len(m2) * getMaskUnity(msat0T0Msk, linAddr);
-        s2 = normalize(m2);
+        ms2 = getMaskUnity(msat0T0Msk, linAddr);
+        m2 = make_double3(mx[linAddr] * ms2, my[linAddr] * ms2, mz[linAddr] * ms2);
 
-        lex2 = getMaskUnity(lexMsk, linAddr) * getMaskUnity(lexMsk, linAddr);
-        pre2 = avgGeomZero(lex02 * ms0, lex2 * ms2);
+        lex = getMaskUnity(lexMsk, linAddr);
+        lex2 =  lex * lex;
+        pre2 = avgGeomZero(lex02, lex2);
         
-        Hx += lex2Mul_cellSizeY2 * (pre1 * (s1.x - s0.x) + pre2 * (s2.x - s0.x));
-        Hy += lex2Mul_cellSizeY2 * (pre1 * (s1.y - s0.y) + pre2 * (s2.y - s0.y));
-        Hz += lex2Mul_cellSizeY2 * (pre1 * (s1.z - s0.z) + pre2 * (s2.z - s0.z));
+        Hx += lex2Mul_cellSizeY2 * (pre1 * (m1.x - m0.x) + pre2 * (m2.x - m0.x));
+        Hy += lex2Mul_cellSizeY2 * (pre1 * (m1.y - m0.y) + pre2 * (m2.y - m0.y));
+        Hz += lex2Mul_cellSizeY2 * (pre1 * (m1.z - m0.z) + pre2 * (m2.z - m0.z));
 
         // Write back to global memory
         hx[I] = msat0T0Mul * Hx;
